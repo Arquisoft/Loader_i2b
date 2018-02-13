29cb39943478807111e598240a43da9798cee3ec
SergioFaya <estisergio@gmail.com>
2018-02-13 17:51:07 +0100

    Deleted the custom exception, was useless having IllegalArgumentException

--- src/main/java/main/asw/util/Checker.java
+++ src/main/java/main/asw/util/Checker.java
@@ -1,21 +1,22 @@
 package main.asw.util;
+
 /**
  * 
- * @author Sergio Faya Fernandez
- * The intend of this class is to group all common field checking methods,
- *  that most of the times will throw runtime exception in case of failure.
+ * @author Sergio Faya Fernandez The intend of this class is to group all common
+ *         field checking methods, that most of the times will throw runtime
+ *         exception in case of failure.
  */
 public class Checker {
 
-	public static void isEmpty(String str) throws WrongParameterException {		
-		if(str.isEmpty()) {
-			throw new WrongParameterException("Value cannot be empty");
+	public static void isEmpty(String str) {
+		if (str.isEmpty()) {
+			throw new IllegalArgumentException("Value cannot be empty");
 		}
 	}
-	
-	public static void isNull(Object o) throws WrongParameterException {
+
+	public static void isNull(Object o) {
 		if (o == null) {
-			throw new WrongParameterException("Value cannot be empty");
+			throw new IllegalArgumentException("Value cannot be empty");
 		}
 	}
 }
