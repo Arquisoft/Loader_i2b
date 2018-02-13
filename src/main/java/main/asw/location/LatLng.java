ad4935171d69b2c08435469c42bc3877ec8048c3
SergioFaya <estisergio@gmail.com>
2018-02-13 15:19:48 +0100

    Added a wrapper for the location of the user. Class LatLng requires latitude and longitude of the user

---
+++ src/main/java/main/asw/location/LatLng.java
@@ -0,0 +1,16 @@
+package main.asw.location;
+/**
+ * 
+ * @author Sergio Faya Fernández
+ *
+ */
+public class LatLng {
+
+	public long latitude;
+	public long longitude;
+	
+	public LatLng(long latitude, long longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+}
