4d61489736bcdc53ad3875750e5dc5e06199025b
SergioFaya <estisergio@gmail.com>
2018-02-25 14:36:18 +0100

    Test inserted in the suite

--- src/test/java/main/asw/AllTests.java
+++ src/test/java/main/asw/AllTests.java
@@ -10,16 +10,18 @@ import main.asw.parser.ParserTest;
 import main.asw.report.ReportWriterTest;
 import main.asw.repository.MongoDBTest;
 import main.asw.repository.PersistenceTest;
+import main.asw.repository.daoTest.DaoTest;
 import main.asw.util.UtilTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
+	DaoTest.class,
 	MainTest.class,
 	AgentsTest.class,
 	EncryptionTest.class,
 	ParserTest.class,
 	ReportWriterTest.class,
-	//MongoDBTest.class,
+	MongoDBTest.class,
 	//PersistenceTest.class,
 	UtilTest.class
 	})
